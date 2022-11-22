86aa9b8750280fec2e2faa279b10813e8293d8f8
Daniel <danielmrg@gmail.com>
2022-11-22 10:30:29 -0300

    Front e back integrados, faltando algumas funcionalidades

---
+++ BackEnd/back/src/test/java/com/daniel/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
